92dce461f9d13fbc774439e3d4727cad04f04f1d
gstolaric <geoffroy.stolaric@maximo.fr>
2020-10-28 12:16:51 +0100

    recode as class object

--- js/main.js
+++ js/main.js
@@ -1,17 +1,21 @@
-let Slide = function(){
-    
-    this.elems = document.querySelectorAll('#content div');
-    this.switchMenu = document.querySelectorAll('a.switch');
-    this.paginationButtons = document.querySelectorAll('.pagination ul li a');
-    this.navigation = document.querySelectorAll('.navigation');
-    this.nbElements = this.elems.length;
-    this.oPageInfo = {
-        page:null,
-        title: null,
-        url: location.href
-    };
+class Slide {
 
-    this.error = function(){
+    constructor() {
+        this.titreSite = document.title;
+        this.pageActive = 0;
+        this.elems = document.querySelectorAll('#content div');
+        this.switchMenu = document.querySelectorAll('a.switch');
+        this.paginationButtons = document.querySelectorAll('.pagination ul li a');
+        this.navigation = document.querySelectorAll('.navigation');
+        this.nbElements = this.elems.length;
+        this.oPageInfo = {
+            page:null,
+            title: null,
+            url: location.href
+        };
+    }
+
+    error = function(){
         let that = this;
         let error = {
             message:"",
@@ -29,7 +33,7 @@ let Slide = function(){
         return error;
     };
 
-    this.getRequest = function(){
+    getRequest = function(){
         let that = this;            
         let queryString = window.location.search;
         let searchParams = new URLSearchParams(queryString);
@@ -46,19 +50,17 @@ let Slide = function(){
         return page;
     };
 
-    this.setDatas = function(element){
-        let that = this;
+    setDatas = function(element){
+        let that = this;        
         let titleSlide = that.elems[element].getAttribute("title");
-        let urlSlide = that.elems[element].getAttribute("url");           
-
-        that.oPageInfo.title = titreSite + " - " + titleSlide;
+        let urlSlide = that.elems[element].getAttribute("url");                   
+        that.oPageInfo.title = that.titreSite + " - " + titleSlide;
         that.oPageInfo.url = urlSlide;
         that.oPageInfo.page=element;
-
         history.pushState(that.oPageInfo, that.oPageInfo.title, that.oPageInfo.url);
     };
 
-    this.setElementVisibility = function(element,visible){
+    setElementVisibility = function(element,visible){
         if(typeof visible === "boolean" && element instanceof HTMLElement){
             if(visible === true){
                 if (element.classList.contains("hidden")){
@@ -72,7 +74,7 @@ let Slide = function(){
         }
     };
 
-    this.bindSwitchMenu = function(){
+    bindSwitchMenu = function(){
         let that = this;
         let navigation = that.navigation[0];
 
@@ -91,13 +93,14 @@ let Slide = function(){
         });
     };
 
-    this.navigationDisplay = function(element){
-        let that = this;
+    navigationDisplay = function(element){
+        let that = this;        
         that.setElementVisibility(that.navigation[0], false);
         let page = window.history.state.page;
         that.navigation[0].querySelectorAll('ul')[0].innerHTML="";
         let i = 0;
         that.elems.forEach(function(contenu){
+            
             var textnode = document.createTextNode(contenu.getAttribute("title"));              
             var li = document.createElement("LI");                 
             var a = document.createElement("A");      
@@ -113,18 +116,18 @@ let Slide = function(){
         });        
     }
 
-    this.bindNavigation = function(link, page){
+    bindNavigation = function(link, page){
         let that = this;
         link.addEventListener('click', function(e){
             e.stopPropagation();
             e.preventDefault();                  
-            pageActive = page;   
+            that.pageActive = page;   
             that.setDatas(page); 
             that.display(page);
         });
     };
 
-    this.setStateNavigation = function(page){
+    setStateNavigation = function(page){
         let that = this;
         // forEachNav method, could be shipped as part of an Object Literal/Module
         var forEachNav = function (array, callback, scope) {
@@ -145,35 +148,35 @@ let Slide = function(){
         }
     };
 
-    this.bindPagination = function(compteur){
+    bindPagination = function(compteur){
         let that = this;
         that.paginationButtons.forEach(function(button){
             button.addEventListener('click', function(e){
                 e.stopPropagation();
                 e.preventDefault();
-                if((button.getAttribute("class") === "prev") && (pageActive !== 0)){
-                    pageActive--;
+                if((button.getAttribute("class") === "prev") && (that.pageActive !== 0)){
+                    that.pageActive--;
                 } 
-                if((button.getAttribute("class") === "next") && (pageActive !== (that.nbElements - 1))){
-                    pageActive++;
+                if((button.getAttribute("class") === "next") && (that.pageActive !== (that.nbElements - 1))){
+                    that.pageActive++;
                 } 
-                that.setDatas(pageActive);                
-                that.display(pageActive);               
+                that.setDatas(that.pageActive);                
+                that.display(that.pageActive);               
             });
         });
     };
 
-    this.navHistory = function(){
+    navHistory = function(){
         let that = this;
         window.onpopstate=function (oEvent){
             if(window.history.state !== null){
-                pageActive = window.history.state.page;
-                that.display(pageActive); 
+                that.pageActive = window.history.state.page;
+                that.display(that.pageActive); 
             }
         };
     }
 
-    this.display = function(element){
+    display = function(element){
         let that = this;
         document.title = window.history.state.title;       
         that.setElementVisibility(that.paginationButtons[0], true);
@@ -198,7 +201,7 @@ let Slide = function(){
         that.setStateNavigation(element);
     };
 
-    this.init = function(){
+    init = function(){
         let that = this;
         let depart = 0;
         if(that.error().reponse === true){
@@ -224,7 +227,7 @@ let Slide = function(){
             depart = request;
         }
 
-        pageActive = depart; 
+        that.pageActive = depart; 
         that.setDatas(depart);                
         that.display(depart);
         that.navigationDisplay(depart); 
@@ -232,13 +235,15 @@ let Slide = function(){
         that.navHistory();
         that.bindSwitchMenu(); 
     };
+
+    create = function (nom) {
+        let that = this;
+        that.titreSite = nom;
+        window.addEventListener ? addEventListener("load", slideLuiggi.init(), false) : window.attachEvent ? attachEvent("onload", slideLuiggi.init()) : (onload = slideLuiggi.init());        
+    }
 };
 
-const titreSite = document.title;
-var pageActive = 0;
 const slideLuiggi = new Slide();
 if(slideLuiggi !== undefined){
-    window.addEventListener ? addEventListener("load", slideLuiggi.init(), false) : window.attachEvent ? attachEvent("onload", slideLuiggi.init()) : (onload = slideLuiggi.init());
+     slideLuiggi.create("Luiggi's Slide");    
 }
-
-
